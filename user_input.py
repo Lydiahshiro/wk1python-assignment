c2f404eea6c8d1ee0e6d0df56881b7c6621b203e
Lydiahshiro <lydiahnjihia4@gmail.com>
2024-02-29 01:01:17 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+print("hello world")
+name = input("what is your name?")
+age = input ("how old are you?")
+location = input ("where do you live?")
+print("name")
+print("age")
+print("location")
+
+
